68e6015827c819da2fb165e65a16c3b14a6d9226
eduardo <edudbdaw@gmail.com>
2026-01-25 15:51:03 +0000

    Pages control and get datas by limit and page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
 
 
-async function getDatas() {
+//page number manage
+let page = 1;
 
+const btnBefore = document.querySelector('#btn-before');
+const btnAfter = document.querySelector('#btn-after');
+const currentPage = document.querySelector('#current-page');
+
+btnBefore.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (page > 1) {
+        page -- ;
+    }
+    getDatas(page);
+    
+})
+
+btnAfter.addEventListener('click',function(e) {
+    e.preventDefault();
+    page++;
+    getDatas(page);
+})
+
+
+function pageNumer(page) {
+    currentPage.textContent = `${page}`
+}
+
+// get datas
+async function getDatas(page) {
+    
+    // I call pageNumer for everyTime i use getDatas , so when i click btn-Before , btn-After or 
+    //by default always the page refresh
+    pageNumer(page);
     try {
-        const api = await fetch("https://api.artic.edu/api/v1/artworks?page=1&limit=2");
+        const api = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}&limit=2`);
         const apiString = await api.json();
         console.log(apiString);
 
@@ -20,6 +51,7 @@ async function getDatas() {
             }
         })
         
+        //show img link
         apiResults.forEach(individualData => {
             console.log(`${urlIIIF}/${individualData.image_id}/full/400,/0/default.jpg`);
         });
@@ -29,4 +61,5 @@ async function getDatas() {
     }
 }
 
-getDatas();
+//Default Charge
+getDatas(page);
